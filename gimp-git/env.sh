a5db4ea237895e6bc4d1586f1cff7fe3773be08f
Andy Keech <w.andrew.keech@gmail.com>
2016-06-06 15:41:38 -0700

    Appending gimp-git work so far

---
+++ gimp-git/env.sh
@@ -0,0 +1,8 @@
+export PREFIX=$HOME/Applications/Gimp/install
+export PATH=$PREFIX/bin:$PATH
+export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
+export XDG_DATA_DIRS=$PREFIX/share:$XDG_DATA_DIRS
+export ACLOCAL_FLAGS="-I $PREFIX/share/aclocal"
+export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH
+export GIO_EXTRA_MODULES=/usr/lib/x86_64-linux-gnu/gio/modules
+export SRC_DIR=$HOME/Applications/Gimp/build
---
+++ gimp-git/env2.sh
@@ -0,0 +1,3 @@
+export PREFIX=$HOME/Applications/Gimp/install
+export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH
+export GIO_EXTRA_MODULES=/usr/lib/x86_64-linux-gnu/gio/modules

c54fa87695f891f05d3edcdb42ef9fd493b32259
Andy Keech <w.andrew.keech@gmail.com>
2016-06-08 11:50:20 -0700

    Still lots of different problems

--- gimp-git/env.sh
+++ gimp-git/env.sh
@@ -1,8 +1,16 @@
-export PREFIX=$HOME/Applications/Gimp/install
-export PATH=$PREFIX/bin:$PATH
-export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
-export XDG_DATA_DIRS=$PREFIX/share:$XDG_DATA_DIRS
-export ACLOCAL_FLAGS="-I $PREFIX/share/aclocal"
-export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH
-export GIO_EXTRA_MODULES=/usr/lib/x86_64-linux-gnu/gio/modules
-export SRC_DIR=$HOME/Applications/Gimp/build
+#!/bin/sh
+
+ARCH='x86_64-linux-gnu'
+
+export LD_LIBRARY_PATH=$SNAP/lib:$LD_LIBRARY_PATH
+export XDG_DATA_DIRS=$SNAP/share:$XDG_DATA_DIRS
+export ACLOCAL_FLAGS="-I $SNAP/share/aclocal"
+export PKG_CONFIG_PATH=$SNAP/lib/pkgconfig:$PKG_CONFIG_PATH
+export GIO_EXTRA_MODULES=/usr/lib/$ARCH/gio/modules
+
+mkdir ./stage/lib 
+mkdir ./stage/lib/pkgconfig 
+mkdir ./stage/share 
+mkdir ./stage/share/aclocal
+
+snapcraft
--- gimp-git/env2.sh
+++
@@ -1,3 +0,0 @@
-export PREFIX=$HOME/Applications/Gimp/install
-export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH
-export GIO_EXTRA_MODULES=/usr/lib/x86_64-linux-gnu/gio/modules

e3a1d494e5f2969273b541590dc270910985f04c
Galileo Sartor <galileo.sartor@gmail.com>
2016-07-14 14:54:53 +0200

    Add upstream texlive installer

---
+++ texworks/parts/plugins/x-texlive.py
@@ -0,0 +1,20 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+import os
+
+import snapcraft
+from snapcraft.plugins import copy
+
+class TexLivePlugin(snapcraft.plugins.copy.CopyPlugin):
+
+    def build(self):
+        super(copy.CopyPlugin, self).build()
+
+        # Install TexLive with the standard installer
+        env = self._build_environment()
+        self.run(['{}/install-tl'.format(self.builddir), '-portable', '-scheme', 'basic'], env=env)
+
+
+    def _build_environment(self):
+        env = os.environ.copy()
+        env['TEXLIVE_INSTALL_PREFIX'] = self.installdir
+        return env

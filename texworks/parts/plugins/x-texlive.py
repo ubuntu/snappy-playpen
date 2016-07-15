0d3cb83d65f1fa19151380a2f54a35841cc12a23
Galileo Sartor <galileo.sartor@gmail.com>
2016-07-15 16:47:10 +0200

    Start texlive install

--- texworks/parts/plugins/x-texlive.py
+++ texworks/parts/plugins/x-texlive.py
@@ -1,5 +1,6 @@
 # -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
 import os
+from subprocess import Popen, PIPE
 
 import snapcraft
 from snapcraft.plugins import copy
@@ -11,7 +12,10 @@ class TexLivePlugin(snapcraft.plugins.copy.CopyPlugin):
 
         # Install TexLive with the standard installer
         env = self._build_environment()
-        self.run(['{}/install-tl'.format(self.builddir), '-scheme', 'basic'], env=env)
+        #self.run(['{}/install-tl'.format(self.builddir), '-scheme', 'basic'], env=env)
+        p1 = Popen(['echo', '-n', 'I'], env=env, stdout=PIPE)
+        p2 = Popen(['{}/install-tl'.format(self.builddir), '-portable', '-scheme', 'basic'], env=env, stdin=p1.stdout, stdout=PIPE)
+        output = p2.communicate()[0]
 
 
     def _build_environment(self):
